4456a4dbd35d296d249598b6f41bf69978406d5b
melisacar <acarme17@itu.edu.tr>
2025-06-17 14:40:01 +0300

    feat: parse product list from API response and print ID and name with selling price

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
+
+type ApiResponse struct {
+	Result struct {
+		Products []Product `json:"products"`
+	} `json:"result"`
+}
+
+type Product struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Price struct {
+		SellingPrice float64 `json:"sellingPrice"`
+	} `json:"price"`
+}
 
 func main() {
 	url := "https://apigw.trendyol.com/discovery-web-searchgw-service/v2/api/infinite-scroll/davlumbaz-x-c103627?pi=1&culture=tr-TR&userGenderId=1&channelId=1"
@@ -21,5 +36,14 @@ func main() {
 		log.Fatal("Error on reading the response", err)
 	}
 
-	fmt.Println(string(body))
+	var apiResp ApiResponse
+
+	err = json.Unmarshal(body, &apiResp)
+	if err != nil {
+		log.Fatal("Error decoding JSON:", err)
+	}
+
+	for _, product := range apiResp.Result.Products {
+		fmt.Printf("Product ID: %d - Name: %s - Price: %.2f\n", product.ID, product.Name, product.Price.SellingPrice)
+	}
 }
